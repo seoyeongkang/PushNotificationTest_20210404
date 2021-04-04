67a02e8a23221d41b04f820ed72691d4dcc5694f
seoyeongkang <jumprope123@naver.com>
2021-04-04 15:59:49 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/tjoeun/pushnotificationtest_20210404/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.tjoeun.pushnotificationtest_20210404
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.tjoeun.pushnotificationtest_20210404", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/tjoeun/pushnotificationtest_20210404/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.tjoeun.pushnotificationtest_20210404
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+
+}
